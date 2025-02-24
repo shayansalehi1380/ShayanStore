17740774cf8e9c602860d09cd55d97949b51a06e
shayansalehi617 <shayansalehi617@gmail.com>
2025-02-24 22:12:06 +0330

    CRUDState&City

---
+++ AdminPanel.UI/Controllers/CityController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Interface;
+using Domain.Entity;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace AdminPanel.UI.Controllers
+{
+    public class CityController(IUnitOfWork unitOfWork) : Controller
+    {
+        public async Task<ActionResult> Create(string name)
+        {
+            await unitOfWork.GenericRepository<City>().AddAsync(new City
+            {
+                Name = name,
+            }, CancellationToken.None);
+            return Ok();
+        }
+
+        public async Task<ActionResult<List<City>>> GetAll()
+        {
+            return await unitOfWork.GenericRepository<City>().TableNoTracking.ToListAsync();
+        }
+
+        public async Task<ActionResult> Update(int id, string name)
+        {
+            var city = await unitOfWork.GenericRepository<City>().GetByIdAsync(id, CancellationToken.None);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            city.Name = name;
+
+            await unitOfWork.GenericRepository<City>().UpdateAsync(city, CancellationToken.None);
+            return Ok(city);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var city = await unitOfWork.GenericRepository<City>().GetByIdAsync(id, CancellationToken.None);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            await unitOfWork.GenericRepository<City>().DeleteAsync(city, CancellationToken.None);
+            return Ok(city);
+        }
+    }
+}
--- AdminPanel.UI/Controllers/StateController.cs
+++ AdminPanel.UI/Controllers/StateController.cs
@@ -7,11 +7,11 @@ namespace AdminPanel.UI.Controllers;
 
 public class StateController(IUnitOfWork unitOfWork) : Controller
 {
-    public async Task<ActionResult> Create(string title)
+    public async Task<ActionResult> Create(string name)
     {
         await unitOfWork.GenericRepository<State>().AddAsync(new State
         {
-            Name = title,
+            Name = name,
         }, CancellationToken.None);
         return Ok();
     }
@@ -19,5 +19,31 @@ public class StateController(IUnitOfWork unitOfWork) : Controller
     public async Task<ActionResult<List<State>>> GetAll()
     {
         return await unitOfWork.GenericRepository<State>().TableNoTracking.ToListAsync();
+    }
+
+    public async Task<ActionResult> Update(int id, string name)
+    {
+        var state = await unitOfWork.GenericRepository<State>().GetByIdAsync(id, CancellationToken.None);
+        if (state == null)
+        {
+            return NotFound();
+        }
+
+        state.Name = name; 
+
+        await unitOfWork.GenericRepository<State>().UpdateAsync(state, CancellationToken.None);
+        return Ok(state);
+    }
+
+    public async Task<ActionResult> Delete(int id)
+    {
+        var state = await unitOfWork.GenericRepository<State>().GetByIdAsync(id, CancellationToken.None);
+        if (state == null)
+        {
+            return NotFound();
+        }
+
+        await unitOfWork.GenericRepository<State>().DeleteAsync(state, CancellationToken.None);
+        return Ok(state);
     }
 }
