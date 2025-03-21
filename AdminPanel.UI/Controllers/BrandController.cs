7777815e44bd9bff56c1daecd5f40425311bc7b7
key1 <keyvan.arjmnd@gmail.com>
2025-03-21 21:14:37 +0330

    change add brand upload imageUri

--- AdminPanel.UI/Controllers/BrandController.cs
+++ AdminPanel.UI/Controllers/BrandController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Common.ApiResult;
+﻿using Application.Common;
+using Application.Common.ApiResult;
 using Application.Interface;
 using Domain.Entity.Products.Brands;
 using Microsoft.AspNetCore.Mvc;
@@ -7,8 +8,10 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace AdminPanel.UI.Controllers
 {
-    public class BrandController(IUnitOfWork unitOfWork) : Controller
+    public class BrandController(IUnitOfWork unitOfWork, IWebHostEnvironment environment) : Controller
     {
+        private Upload upload = new Upload(environment);
+
         public async Task<ActionResult<List<Brand>>> GetAllBrand(string searchBrand, int tabs = 1)
         {
             IQueryable<Brand> queryBrand = unitOfWork.GenericRepository<Brand>()
@@ -26,33 +29,23 @@ namespace AdminPanel.UI.Controllers
 
         public async Task<ActionResult<Brand>> Create(string name, IFormFile image)
         {
-            // بررسی وجود تصویر  
-            if (image != null && image.Length > 0)
+            if (image != null)
             {
-                // تبدیل تصویر به Base64
-                using (var memoryStream = new MemoryStream())
-                {
-                    await image.CopyToAsync(memoryStream);
-                    var fileBytes = memoryStream.ToArray();
-                    var base64String = Convert.ToBase64String(fileBytes);
-
-                    await unitOfWork.GenericRepository<Brand>().AddAsync(new Brand
-                    {
-                        Title = name,
-                        ImageUri = base64String
-                    }, CancellationToken.None);
-
-                    return RedirectToAction("ManageBrand", "Admin", new { tabs = 1, status = FunctionStatus.Success });
-                }
+                return BadRequest("لطفا یک تصویر آپلود کنید.");
             }
 
-            // در صورت عدم وجود تصویر، می‌توانید مدیریت کنید  
-            return BadRequest("لطفا یک تصویر آپلود کنید.");
+            await unitOfWork.GenericRepository<Brand>().AddAsync(new Brand
+            {
+                Title = name,
+                ImageUri = upload.UploadFile(image, "Brands")
+            }, CancellationToken.None);
+            return RedirectToAction("ManageBrand", "Admin", new { tabs = 1, status = FunctionStatus.Success });
         }
 
         public async Task<ActionResult<Brand>> Update(int id, string name, IFormFile image)
         {
-            var brand = await unitOfWork.GenericRepository<Brand>().Table.FirstOrDefaultAsync(x => x.Id == id, CancellationToken.None);
+            var brand = await unitOfWork.GenericRepository<Brand>().Table
+                .FirstOrDefaultAsync(x => x.Id == id, CancellationToken.None);
 
             if (brand == null)
             {
@@ -60,18 +53,7 @@ namespace AdminPanel.UI.Controllers
             }
 
             brand.Title = name;
-
-            // اگر تصویر جدید آپلود شده است
-            if (image != null && image.Length > 0)
-            {
-                // تبدیل تصویر به Base64
-                using (var memoryStream = new MemoryStream())
-                {
-                    await image.CopyToAsync(memoryStream);
-                    var fileBytes = memoryStream.ToArray();
-                    brand.ImageUri = Convert.ToBase64String(fileBytes); // ذخیره به‌صورت Base64
-                }
-            }
+            brand.ImageUri = image != null ? upload.UploadFile(image, "Brands") : brand.ImageUri;
 
             await unitOfWork.GenericRepository<Brand>().UpdateAsync(brand, CancellationToken.None);
             return RedirectToAction("ManageBrand", "Admin", new { tabs = 1, status = FunctionStatus.Success });
@@ -79,7 +61,8 @@ namespace AdminPanel.UI.Controllers
 
         public async Task<ActionResult<Brand>> SoftDelete(int id)
         {
-            var brand = await unitOfWork.GenericRepository<Brand>().Table.FirstOrDefaultAsync(x => x.Id == id, CancellationToken.None);
+            var brand = await unitOfWork.GenericRepository<Brand>().Table
+                .FirstOrDefaultAsync(x => x.Id == id, CancellationToken.None);
 
             if (brand == null)
             {
@@ -90,12 +73,12 @@ namespace AdminPanel.UI.Controllers
 
             await unitOfWork.GenericRepository<Brand>().UpdateAsync(brand, CancellationToken.None);
             return RedirectToAction("ManageBrand", "Admin", new { tabs = 1, status = FunctionStatus.Success });
-
         }
 
         public async Task<ActionResult<Brand>> Delete(int id)
         {
-            var brand = await unitOfWork.GenericRepository<Brand>().Table.FirstOrDefaultAsync(x => x.Id == id, CancellationToken.None);
+            var brand = await unitOfWork.GenericRepository<Brand>().Table
+                .FirstOrDefaultAsync(x => x.Id == id, CancellationToken.None);
 
             if (brand == null)
             {
