d941114236d6f2e5f2cf600e55fe85502b030a54
shayansalehi617 <shayansalehi617@gmail.com>
2025-03-27 12:51:56 +0330

    EditProductStatus

--- Domain/Enums/ProductStatus.cs
+++ Domain/Enums/ProductStatus.cs
@@ -6,10 +6,10 @@ public enum ProductStatus
 {
     [Display(Name = "موجود")]
     Available=0,
-    [Display(Name = "غیر موجود")]
+    [Display(Name = "ناموجود")]
     NotAvailable=1,
     [Display(Name = "به زودی")]
     ComingSoon=2,
     [Display(Name = "پیش فروش")]
-    OutOfDate=3
+    PreSale = 3
 }
